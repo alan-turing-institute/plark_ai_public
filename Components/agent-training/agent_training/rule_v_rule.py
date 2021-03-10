8b0cedf59359ba6410df5d896269b807f1cda3a3
Rahul Savani <rahul.savani@gmail.com>
2021-03-10 09:41:47 +0000

    pelicanAgent_any_size_grid.py config used in rule vs rule

--- Components/agent-training/agent_training/rule_v_rule.py
+++ Components/agent-training/agent_training/rule_v_rule.py
@@ -8,7 +8,8 @@ from plark_game.classes.rule_based_game import create_rule_based_game
 if __name__ == '__main__':
 
     #Env variables
-    config_file_path = '/Components/plark-game/plark_game/game_config/10x10/balanced_DEBUG.json'
+    # config_file_path = '/Components/plark-game/plark_game/game_config/10x10/balanced_DEBUG.json'
+    config_file_path = '/Components/plark-game/plark_game/game_config/10x10/balanced_pelicanAgent_any_size_grid.json'
     driving_agent = 'pelican'
     ui_on = False
 
